6b1275e40aeb889fc05a4fd833582d019e962ac9
Brodie Knight <nk624@login-p-3.data.cluster>
2026-05-13 11:06:30 +0100

    updated scripts

--- scripts/test_infer_overfit.sh
+++ scripts/test_infer_overfit.sh
@@ -1,1 +1,1 @@
-python fvm_foundation/infer.py fvm_foundation/checkpoints/model-epoch=057-train_loss=0.03995.ckpt out/test_infer_overfit -r 1 --data-dir data/fvm_gen_overfit
+python fvm_foundation/infer.py fvm_foundation/checkpoints/model-epoch=006-train_loss=0.17219.ckpt out/test_infer_overfit -r 1 --data-dir data/fvm_gen_overfit
